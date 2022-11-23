2f720d5b6651da1025cf5f50600ba83fb201ceab
m-njora <mmuthekijora@gmail.com>
2022-11-23 11:10:50 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
+// function multiply(){
+//     const num1 = 31
+//     const num2 = 2
+//     return num1*num2
+// }
+//    console.log(num1*num2) 
+    
+    
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+const multiply = num1*num2
+
+const random =  1
+
+const num3 = 16
+const num4 = 4
+const mod = num3/num4
+
+const max = 20
+
